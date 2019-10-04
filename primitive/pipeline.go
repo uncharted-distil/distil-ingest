cc3c6975d485d4218b0ef25c7ac5ad1c76af2204
philippe horne <phorne@uncharted.software>
2019-10-04 12:53:27 -0400

    Fixed calls to execute pipeline.

--- primitive/pipeline.go
+++ primitive/pipeline.go
@@ -70,7 +70,7 @@ func NewIngestStep(client *compute.Client) *IngestStep {
 
 func (s *IngestStep) submitPrimitive(datasets []string, step *pipeline.PipelineDescription) (string, error) {
 
-	res, err := s.client.ExecutePipeline(context.Background(), datasets, step)
+	res, err := s.client.ExecutePipeline(context.Background(), datasets, step, true)
 	if err != nil {
 		return "", errors.Wrap(err, "unable to dispatch mocked pipeline")
 	}
